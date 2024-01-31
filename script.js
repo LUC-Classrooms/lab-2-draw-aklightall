2f314d450698b74fcbe7ad079563c28e65949b75
aklightall <156939550+aklightall@users.noreply.github.com>
2024-01-31 20:18:34 +0000

    Updated shapes

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ function setup(){
 function draw(){
   background(200);
   // add your drawing code here ...
-  fill (290,900,30)
+  fill (240,700,50)
   ellipse(150, 50, 50)
-  triangle(110, 200, 30, 20, 100, 100);
-}
+  triangle(110, 300, 20, 20, 200, 200);
+  rect(40, 20, 50, 50)
+line(30, 50, 70, 90)
+} 
+
